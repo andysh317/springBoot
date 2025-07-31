9c6c269bd7dc8c82f94fae13f55c346a11971599
unknown <andysh317@gmail.com>
2025-07-31 14:23:01 +0530

    practise commit 2

--- eureka-server/src/main/java/com/example/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eureka/EurekaServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args) {
-    	 System.out.println("Test");
+    	 System.out.println("Test");//Latest INT
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
